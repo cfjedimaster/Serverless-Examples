3b6ef9661211f14f485572732a97e0c37449ff7c
Raymond Camden <raymondcamden@gmail.com>
2017-04-10 06:18:58 -0500

    minor tweak

--- elasticsearch/upd.bat
+++ elasticsearch/upd.bat
@@ -1,3 +1,7 @@
 del search.zip
 7z a -r search.zip search.js package.json node_modules/*
 wsk action update elasticsearch/search --kind nodejs:6 .\search.zip
+
+del create.zip
+7z a -r create.zip create.js package.json node_modules/*
+wsk action update elasticsearch/create --kind nodejs:6 .\create.zip

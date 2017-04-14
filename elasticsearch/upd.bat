48c64ae0070b929100a2e67c62a4c1ef7c645479
Raymond Camden <raymondcamden@gmail.com>
2017-04-14 11:21:14 -0500

    better update

--- elasticsearch/upd.bat
+++ elasticsearch/upd.bat
@@ -1,7 +1,14 @@
 del search.zip
+copy package.search.json package.json
 7z a -r search.zip search.js package.json node_modules/*
 wsk action update elasticsearch/search --kind nodejs:6 .\search.zip
 
 del create.zip
+copy package.create.json package.json
 7z a -r create.zip create.js package.json node_modules/*
 wsk action update elasticsearch/create --kind nodejs:6 .\create.zip
+
+del bulk.zip
+copy package.bulk.json package.json
+7z a -r bulk.zip bulk.js package.json node_modules/*
+wsk action update elasticsearch/bulk --kind nodejs:6 .\bulk.zip

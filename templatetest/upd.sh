c1d2d1cf8a7df047c76fe89ad3681228e3aaf2d5
Raymond Camden <raymondcamden@gmail.com>
2017-04-21 11:09:09 -0500

    upd

--- templatetest/upd.sh
+++ templatetest/upd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 zip -rq temp.zip test1.js package.json node_modules templates
-wsk action update templatetest/test --kind nodejs:6 temp.zip
+wsk action update templatetest/test --kind nodejs:6 temp.zip --web true
 rm temp.zip

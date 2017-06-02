57df00aaa7131e3c643532e26b4f2d3396d57dd8
Raymond Camden <raymondcamden@gmail.com>
2017-06-02 09:05:03 -0500

    updated demo code

---
+++ nodeexample/actions/getcats.js
@@ -0,0 +1,10 @@
+function main(args) {
+	let cats = ["Miss Serverless","Robin", "Cracker", "Luna", "Pig"];
+
+	return { cats };
+
+}
+
+exports.main = main;
+
+
--- nodeexample/dataService.js
+++ nodeexample/dataService.js
@@ -1,16 +1,24 @@
-module.exports = {
-
-	getCats:function() {
-		return new Promise((resolve) => {
-			let cats = ["Robin", "Cracker", "Luna", "Pig"];
-			resolve(cats);
-		});
-	},
-	getDogs:function() {
-		return new Promise((resolve) => {
-			let cats = ["Cayenne", "Panda Express", "Kimchee"];
-			resolve(cats);
-		});
-	},
-	
+const openwhisk = require('openwhisk');
+const options = {
+	apihost:'openwhisk.ng.bluemix.net',
+	api_key:'super_secret_key_no_one_will_guess'
 }
+const ow = openwhisk(options);
+
+function getCats() {
+//	return ow.actions.invoke({name:'safeToDelete/getcats',blocking:true,result:true});
+	return new Promise((resolve, reject) => {
+		ow.actions.invoke({name:'safeToDelete/getcats',blocking:true,result:true}).then((result)=>{
+			resolve(result.cats);
+		});
+	});
+}
+
+function getDogs() {
+	return new Promise((resolve) => {
+		let cats = ["Cayenne", "Panda Express", "Kimchee"];
+		resolve(cats);
+	});
+}
+
+module.exports = { getCats, getDogs }

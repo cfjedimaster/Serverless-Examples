cbb12bb3c72ec69c3225c586492f29d391df7e69
Raymond Camden <raymondcamden@gmail.com>
2017-06-29 08:51:30 -0500

    forgot to do this too

---
+++ bname/pig.js
@@ -0,0 +1,24 @@
+ // source: https://github.com/montanaflynn/piglatin
+ function piglatin(text) {
+  var words = text.split(/\W+/)
+  var piggish = ""
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i]
+    var firstLetter = word.charAt(0)
+    if (word.length > 2) {
+      piggish = piggish + word.substring(1) + firstLetter + "ay "
+    } else {
+      piggish = piggish + word + " "
+    }
+  }
+  return piggish.toLowerCase().trim();
+}
+
+function main(args) {
+  
+  if(!args.input) return { error:'Input argument required.'};
+
+	let result = piglatin(args.input);
+	return { result:result};
+
+}

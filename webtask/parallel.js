056db9a204826b945cda892858eb0441c3602340
Raymond Camden <raymondcamden@gmail.com>
2018-03-07 14:36:15 -0600

    test

--- webtask/parallel.js
+++ webtask/parallel.js
@@ -1,6 +1,9 @@
 module.exports = function (options, cb) {
 	console.log('compiler called', options.script);
+	// assume its a json array
+	let seq = JSON.parse(options.script);
+
     return cb(null, function (cb) {
-      cb(null, options.script);
+      cb(null, "I was sent "+seq.length+" items.");
     });
 };

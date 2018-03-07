f03d5551b4c74518f3190ed2da28c452fd3b7e45
Raymond Camden <raymondcamden@gmail.com>
2018-03-07 15:56:22 -0600

    work

--- webtask/parallel.js
+++ webtask/parallel.js
@@ -22,9 +22,17 @@ module.exports = function (options, cb) {
 			console.log('in the All for fetching them all.');
 			console.log('result should be an arr i think? ',result.length);
 			console.log('test', req.url);
+			/*
 			for(var x in req) {
 				console.log('req',x);
 			}
+			*/
+			let proto = req.headers['x-forwarded-proto']
+            ? req.headers['x-forwarded-proto']
+            : 'https';
+			let baseUrl = `${proto}://${req.headers.host}/${req.x_wt.container}/`;
+			console.log('new test proto='+proto+' and b='+baseUrl);
+			
 			res.setHeader('Content-Type', 'application/json');
 			res.end(JSON.stringify(result));
 		})

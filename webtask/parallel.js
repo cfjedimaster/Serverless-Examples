ccba4b4705c422c95651c1af5171009b3dc3ea2e
Raymond Camden <raymondcamden@gmail.com>
2018-03-07 16:03:08 -0600

    work

--- webtask/parallel.js
+++ webtask/parallel.js
@@ -7,31 +7,28 @@ module.exports = function (options, cb) {
 	// assume its a json array
 	let seq = JSON.parse(options.script);
 
-	seq = ['https://www.raymondcamden.com','https://www.cnn.com'];
+	//seq = ['https://www.raymondcamden.com','https://www.cnn.com'];
 
 	return cb(null, (context, req, res) => {
 
 		console.log('entered CB');
 		let promises = [];
 
+		// def not 100% sure
+		let proto = req.headers['x-forwarded-proto']
+		? req.headers['x-forwarded-proto']
+		: 'https';
+		let baseUrl = `${proto}://${req.headers.host}/`;
+		console.log('new testand b='+baseUrl);
+
 		seq.forEach(u => {
-			promises.push(rp(u));
+			promises.push(rp(baseUrl+u));
 		});
 		console.log('pushed '+promises.length+' items');
 		Promise.all(promises).then(result => {
 			console.log('in the All for fetching them all.');
 			console.log('result should be an arr i think? ',result.length);
 			console.log('test', req.url);
-			/*
-			for(var x in req) {
-				console.log('req',x);
-			}
-			*/
-			let proto = req.headers['x-forwarded-proto']
-            ? req.headers['x-forwarded-proto']
-            : 'https';
-			let baseUrl = `${proto}://${req.headers.host}/${req.x_wt.container}/`;
-			console.log('new test proto='+proto+' and b='+baseUrl);
 			
 			res.setHeader('Content-Type', 'application/json');
 			res.end(JSON.stringify(result));

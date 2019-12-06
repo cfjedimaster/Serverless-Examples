aa076a86edc92745abcfc7616711bebe9f739667
Raymond Camden <raymondcamden@gmail.com>
2019-12-06 15:23:23 -0600

    new logic

--- antibot2/main.js
+++ antibot2/main.js
@@ -5,13 +5,47 @@ function getRandomInt(min, max) {
 function getWeather() {
 	let responses = [
 		"Go outside and look up in the sky you fucking idiot!",
-		"Hot, cold, who the fuck cares, you're probably too lazy to go outside anyway!"
+		"Hot, cold, who the fuck cares, you're probably too lazy to go outside anyway!",
+		"Look out a god damn window."
+	];
+	return responses[getRandomInt(0, responses.length-1)];
+}
+
+function getFallback() {
+	let responses = [
+		"I have no idea what you fucking said, idiot.",
+		"Eh? Speak up - I can't hear you over the sound of you being stupid."
+	];
+	return responses[getRandomInt(0, responses.length-1)];
+}
+
+function getFortune() {
+	let responses = [
+		"You're going to die",
+		"You will die horribly in a massive wreck where you shit your pants.",
+		"You will die alone and miserable.",
+		"You have no future."
+	];
+	return responses[getRandomInt(0, responses.length-1)];
+}
+
+function getHelp() {
+	let responses = [
+		"Damn right you need help.",
+		"You are far beyond any help.",
+		"I'd rather eat my own vomit."
+	];
+	return responses[getRandomInt(0, responses.length-1)];
+}
+
+function getName() {
+	let responses = [
+		"Fuck you, that's my name!"
 	];
 	return responses[getRandomInt(0, responses.length-1)];
 }
 
 function main(params) {
-	//console.log('called', JSON.stringify(params.request));
 	let intent = '';
 	if(params.request && params.request.intent) intent = params.request.intent.name;
 	console.log(`intent=${intent}`);
@@ -24,9 +58,21 @@ function main(params) {
 			text = getWeather();
 			break;
 		
+		case "name":
+			text = getName();
+			break;
+
+		case "fortune":
+			text = getFortune();
+			break;
+
+		case "AMAZON.HelpIntent":
+			text = getHelp();
+			break;
+
 		//Fallback intent basically
 		default: 
-			text = 'I have no idea what you fucking said, idiot.';
+			text = getFallback();
 		
 	}
 
